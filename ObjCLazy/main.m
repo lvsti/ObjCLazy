fd8447d5654e50fbb5b919fc7f7372aad53febf7
Lk Xf <elveestei@gmail.com>
2014-05-19 21:54:09 +0200

    Initial Commit

---
+++ ObjCLazy/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjCLazy
+//
+//  Created by Tamas Lustyik on 2014.05.19..
+//  Copyright (c) 2014 LKXF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
